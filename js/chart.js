b1f2004c730183b5440ae2ac51af635f4efdef40
can-dy-jack <jjack_chen3@163.com>
2021-04-01 16:10:44 +0800

    hight-Chart

--- js/chart.js
+++ js/chart.js
@@ -1,118 +1,48 @@
-var ctx = document.getElementById('Chart2').getContext('2d');
-var myChart = new Chart(ctx, {
+var ctxBar = document.getElementById('chart-bar').getContext('2d');
+var chartBar = new Chart(ctxBar, {
     type: 'bar',
     data: {
-        labels: ['html', 'css', 'js', 'bootstrap', 'jQeury', 'vue','react'],
+        labels: ['2015', '2016', '2017', '2018', '2019'],
         datasets: [{
-            label: '大小',
-            data: [12, 19, 8, 10, 5, 5,1],
+            label: '海洋生产总值（亿元）',
+            data: [65534, 69694, 76749, 83415, 89415],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(23,15,250,0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(23,15,250,1)'
-            ],
-            borderWidth: 2
+                'rgba(255, 99, 132, 0.6)',
+                'rgba(54, 162, 235, 0.6)',
+                'rgba(255, 206, 86, 0.6)',
+                'rgba(75, 192, 192, 0.6)',
+                'rgba(153, 102, 255, 0.6)'
+                //'rgba(255, 159, 64, 0.6)'
+            ]
         }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
     }
 });
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
+var ctxLine = document.getElementById('chart-line');
+var chartLine = new Chart(ctxLine, {
     type: 'line',
     data: {
-        labels: ['html', 'css', 'js', 'bootstrap', 'jQeury', 'vue','react'],
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
         datasets: [{
-            label: '语言掌握程度',
-            data: [12, 19, 8, 10, 5, 5,1],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(23,15,250,0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
-                'rgba(23,15,250,1)'
-            ],
-            borderWidth: 2
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
-
-var ctx = document.getElementById('Chart4').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'polarArea',
-    data: {
-        labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
-		datasets: [
-		{
-		label: "My First dataset",
-		fillColor: "rgba(220,220,220,0.2)",
-		strokeColor: "rgba(220,220,220,1)",
-		pointColor: "rgba(220,220,220,1)",
-		pointStrokeColor: "#fff",
-		pointHighlightFill: "#fff",
-		pointHighlightStroke: "rgba(220,220,220,1)",
-		data: [65, 59, 90, 81, 56, 55, 40]
-		},
-		{
-		label: "My Second dataset",
-		fillColor: "rgba(151,187,205,0.2)",
-		strokeColor: "rgba(151,187,205,1)",
-		pointColor: "rgba(151,187,205,1)",
-		pointStrokeColor: "#fff",
-		pointHighlightFill: "#fff",
-		pointHighlightStroke: "rgba(151,187,205,1)",
-		data: [28, 48, 40, 19, 96, 27, 100]
-		}
-		]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+                label: "s",
+                fillColor: "rgba(54, 162, 235, 0.6)",
+                strokeColor: "rgba(54, 162, 235, 1)",
+                pointColor: "rgba(54, 162, 235,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(54, 162, 235,1)",
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            {
+                label: "v",
+                fillColor: "rgba(75, 192, 192, 0.6)",
+                strokeColor: "rgba(75, 192, 192,1)",
+                pointColor: "rgba(75, 192, 192,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(75, 192, 192,1)",
+                data: [28, 48, 40, 19, 86, 27, 90]
+            }
+        ]
     }
 });
